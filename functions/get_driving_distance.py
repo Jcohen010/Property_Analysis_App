c5381236a81deb6275ab7e71aed5b77d9d621ff3
Justin Cohen <justincohen@justins-air.lan>
2023-03-06 18:21:33 -0500

    change function to accomodate change in dictionary structure

--- functions/get_driving_distance.py
+++ functions/get_driving_distance.py
@@ -15,22 +15,22 @@ def get_driving_distance(dict):
                     propertylat = place['lat']
                     propertylon = place['lon']
 
-    for coord in dict['coordinates']:
-        for place in coord:
-            if place != 'property':
-                person = place
-                for place in coord.values():
-                    lat = place['lat']
-                    lon = place['lon']
+    for category, dictionary in dict['coordinates'].items():
+        for item in category:
+            for place, coordinatepair in item.items():
+                if place != 'property':
+                    person = place
+                lat = coordinatepair['lat']
+                lon = coordinatepair['lon']
 
-                    r = requests.get(f"http://router.project-osrm.org/route/v1/car/{propertylon},{propertylat};{lon},{lat}?overview=false""")
-                    # then you load the response using the json libray
-                    # by default you get only one alternative so you access 0-th element of the `routes`
-                    routes = json.loads(r.content)
-                    route_1 = routes.get("routes")[0]
+                r = requests.get(f"http://router.project-osrm.org/route/v1/car/{propertylon},{propertylat};{lon},{lat}?overview=false""")
+                # then you load the response using the json libray
+                # by default you get only one alternative so you access 0-th element of the `routes`
+                routes = json.loads(r.content)
+                route_1 = routes.get("routes")[0]
 
-                    distance = int(route_1["duration"]/60)
+                distance = int(route_1["duration"]/60)
 
-                    distance_list.append(f"{person}: {distance} Mins")
+                distance_list.append(f"{person}: {distance} Mins")
 
     return distance_list
